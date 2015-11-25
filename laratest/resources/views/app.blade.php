be73d600fb9b1b8c016171e62dcb941d7185953b
peter <peter@agilizelabs.com>
2015-11-26 01:30:30 +0530

    updated css

--- laratest/resources/views/app.blade.php
+++ laratest/resources/views/app.blade.php
@@ -4,13 +4,37 @@
 
 <head>
 	<meta charset="UTF-8"></meta>
-	<title>My First App</title>
-	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<title>My Blog</title>
+	{!! Html::style('css/all.css') !!}
 </head>
 
 
 
 <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="{!! URL::to('/') !!}">My Blog</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="{!! URL::to('/articles') !!}">Articles</a></li>
+            <li><a href="{!! URL::to('/articles/create') !!}">Create Article</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+
 	<div class="container">
 		@include('flash::message')
 
@@ -22,8 +46,7 @@
 		$('#flash-overlay-modal').modal();
 		//$('div.alert').not('.alert-important').delay(3000).slideUp(300);
 	</script>
-
-	<script src="../js/all.js"></script>
+	{!! Html::script('js/all.js') !!}
 	@yield('footer')
 </body>
 
